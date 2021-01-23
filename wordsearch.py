4a6905df127b6ab5473fe5fa89ffc2aa655e2473
Michael Penkov <misha.penkov@gmail.com>
2021-01-23 22:17:50 +0900

    implement downloader

--- wordsearch.py
+++ wordsearch.py
@@ -2,11 +2,35 @@
 
 See http://bokrcorpora.narod.ru/frqlist/frqlist-en.html
 """
+import io
+import os
 import sys
+import urllib.request
+import zipfile
+
+URL = 'http://bokrcorpora.narod.ru/frqlist/lemma.al.zip'
+LEMMA = 'lemma.al'
+
+if not os.path.isfile(LEMMA):
+    #
+    # Not sure why zifile cannot read from fin directly, but it
+    # appears to think the stream does not contain a zip file.
+    #
+    buf = io.BytesIO()
+    with urllib.request.urlopen(URL) as fin:
+        buf.write(fin.read())
+
+    with zipfile.ZipFile(buf) as zf:
+        with zf.open(LEMMA, 'r') as fin, open(LEMMA, 'wb') as fout:
+            fout.write(fin.read())
+
+
+assert os.path.isfile(LEMMA)
+
 
 nouns = []
 
-with open('lemma.al', encoding='windows-1251') as fin:
+with open(LEMMA, encoding='windows-1251') as fin:
     fin.readline()
     for line in fin:
         word_rank, frequency, word, pos = line.strip().split(' ')
